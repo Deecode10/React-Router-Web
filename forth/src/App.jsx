2b893d25e34e78b9d0bb1cf2b3c247286df5b3ca
Deecode10 <160780418+Deecode10@users.noreply.github.com>
2024-03-05 23:39:50 +0530

    Initial commit

---
+++ forth/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(white)
+
+  return (
+    
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor:color}}>
+<div className="fixed flex flex-wrap justify-content bottom-12 inset-x-0 px-2">
+  <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"yellow"}}>yellow</button>
+    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}}>blue</button>
+    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>green</button>
+  </div>
+</div>
+<h1>deepak</h1>
+      </div>
+    
+  )
+}
+
+export default App
